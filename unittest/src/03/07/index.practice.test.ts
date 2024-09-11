27b6a9df4af67fa751a0367df1413dc4c4bea9ed
koyaaoyama <aoyama.ko0920@gmail.com>
2024-09-11 22:29:11 +0900

    Promise unit test

--- unittest/src/03/07/index.practice.test.ts
+++ unittest/src/03/07/index.practice.test.ts
@@ -1,13 +1,12 @@
 import { timeout, wait } from ".";
 
-describe("非同期処理のテスト", () => {
-  test("resolve", async () => {
-    expect(wait(50)).resolves.toBe(50);
+describe("Promise test", () => {
+  test("waitがdurationの値でresolveされる", async () => {
+    await expect(wait(50)).resolves.toBe(50);
   });
 
-  test("reject", async () => {
+  test("timeoutがdurationの値でrejectされる", async () => {
     expect.assertions(1);
-
     try {
       await timeout(50);
     } catch (err) {
