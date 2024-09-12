ce9eada125dfe5f3c2c115eb0188dbea099dde6a
koyaaoyama <aoyama.ko0920@gmail.com>
2024-09-12 22:28:54 +0900

    ArticleListItem component test

--- unittest/src/05/04/ArticleListItem.practice.test.tsx
+++ unittest/src/05/04/ArticleListItem.practice.test.tsx
@@ -3,9 +3,11 @@ import { ArticleListItem } from "./ArticleListItem";
 import { items } from "./fixture";
 
 describe("ArticleListItem", () => {
-  test("IDに紐づいたリンクが表示される", () => {
+  test("title, body, リンクが表示される", () => {
     const item = items[0];
     render(<ArticleListItem {...item} />);
+    expect(screen.getByRole("heading")).toHaveTextContent(item.title);
+    expect(screen.getByText(item.body)).toBeInTheDocument();
     expect(screen.getByRole("link", { name: "もっと見る" })).toHaveAttribute(
       "href",
       `/articles/${item.id}`
