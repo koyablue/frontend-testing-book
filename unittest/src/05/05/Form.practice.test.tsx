f007f3b7771ff6e74403f3232d76cd027cde5a03
koyaaoyama <aoyama.ko0920@gmail.com>
2024-09-12 23:26:09 +0900

    Form component test

--- unittest/src/05/05/Form.practice.test.tsx
+++ unittest/src/05/05/Form.practice.test.tsx
@@ -4,25 +4,30 @@ import { Form } from "./Form";
 
 const user = userEvent.setup();
 
-describe("Form component", () => {
-  test("利用規約がチェックされていない場合サインアップボタンは非活性", () => {
-    render(<Form />);
-    expect(screen.getByRole("checkbox")).not.toBeChecked();
-    expect(
-      screen.getByRole("button", { name: "サインアップ" })
-    ).not.toBeEnabled();
+beforeEach(() => {
+  render(<Form />);
+});
+
+describe("Form", () => {
+  test("アカウント情報が入力できる", async () => {
+    const emailInput = screen.getByRole("textbox", { name: "メールアドレス" });
+    await user.type(emailInput, "example@example.com");
+    expect(emailInput).toHaveDisplayValue("example@example.com");
+
+    const passwordInput = screen.getByPlaceholderText("8文字以上で入力");
+    await user.type(passwordInput, "passw0rD");
+    expect(passwordInput).toHaveDisplayValue("passw0rD");
   });
-  test("利用規約がチェックされている場合サインアップボタンは活性", async () => {
-    render(<Form />);
-    const checkbox = screen.getByRole("checkbox");
+
+  test("チェックボックスをチェックしていないときボタンが非活性", async () => {
+    const checkbox = screen.getByRole("checkbox", {
+      name: "当サービスの 利用規約 を確認し、これに同意します",
+    });
+    const button = screen.getByRole("button", { name: "サインアップ" });
+    expect(checkbox).not.toBeChecked();
+    expect(button).not.toBeEnabled();
     await user.click(checkbox);
     expect(checkbox).toBeChecked();
-    expect(screen.getByRole("button", { name: "サインアップ" })).toBeEnabled();
-  });
-  test("formのアクセシブルロールはh2を引用している", () => {
-    render(<Form />);
-    expect(
-      screen.getByRole("form", { name: "新規アカウント登録" })
-    ).toBeInTheDocument();
+    expect(button).toBeEnabled();
   });
 });
